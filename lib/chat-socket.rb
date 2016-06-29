b26baf9ed6f647849f3d46ce1bd1e1d058ca5775
Nathan Reed <reednj@gmail.com>
2016-06-29 16:19:14 +0800

    make sure to send correct counts when disconnecting

--- lib/chat-socket.rb
+++ lib/chat-socket.rb
@@ -21,6 +21,16 @@ class ChatWebSocket < WebSocketHelper
 		super
 		log_action 'chat_connect'
 		self.send_system_chat "#{chatting_count} chatting, #{connected_count-chatting_count} watching"
+		self.send_counts
+	end
+
+	def on_close
+		self.send_room_others 'countChanged', {
+			:connected => connected_count - 1,
+			:chatting => chatting_count - (can_chat? ? 1 : 0)
+		}
+
+		super
 	end
 
 	def on_chat(data)
@@ -31,21 +41,32 @@ class ChatWebSocket < WebSocketHelper
 	end
 
 	def chatting_count
-		@sockets.select{|s| s.can_chat? }.length
+		room_sockets.select{|s| s.can_chat? }.length
 	end
 
 	def connected_count
-		@sockets.length
+		room_sockets.length
 	end
 
 	def can_chat?
 		!self.username.nil?
 	end
 
+	def room_sockets
+		@sockets.select{|s| s.room == self.room }
+	end
+
 	def log_action(action_name, options={})
 		options[:username] = self.username
 		options[:thread_id] = self.room
 		ActionLog.log action_name, options
+	end
+
+	def send_counts
+		self.send_room 'countChanged', {
+			:connected => connected_count,
+			:chatting => chatting_count
+		}
 	end
 
 	def send_system_chat(message)
@@ -55,10 +76,18 @@ class ChatWebSocket < WebSocketHelper
 		}
 	end
 
+	def send_room_others(event, data)
+		EM.next_tick do
+			room_sockets.each do |s|
+				next if s == self
+				s.send event, data
+			end
+		end
+	end
+
 	def send_room(event, data)
 		EM.next_tick do
-			@sockets.each do |s|
-				next if s.room != self.room
+			room_sockets.each do |s|
 				s.send event, data
 			end
 		end
