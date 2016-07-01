89ef8d226694666f27a66a0f21ca5f5ad8320a72
Nathan Reed <reednj@gmail.com>
2016-07-01 14:29:14 +0800

    fix some problems with the deploy script

--- config/deploy.sh
+++ config/deploy.sh
@@ -4,13 +4,14 @@ WEB=~/chat.reednj.com
 SRC=~/code/chat.git
 CONFIG=~/code/config_backup/redditstream
 
-# create tmp directory
-mkdir $WEB/tmp
-
 # copy the required files to the website
 rm -rf $WEB/*
 cp -R $SRC/* $WEB
 cp $CONFIG/app.config.rb $WEB/config/
+
+# create tmp directory
+mkdir $WEB/tmp
+mkdir $WEB/public
 
 # restart the server
 sudo -u www-data thin --config /etc/thin/chat.reednj.com.yml restart > $WEB/tmp/thin-restart.log &
